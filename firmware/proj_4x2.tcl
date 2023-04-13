e4d5d4cc68ddc168190a08e288b78bcddd0d2ff0
Leandro Stefanazzi <lstefana@fnal.gov>
2023-04-13 14:07:16 -0500

    added project files for new tproc_v2

--- firmware/proj_4x2.tcl
+++ firmware/proj_4x2.tcl
@@ -43,7 +43,7 @@ update_ip_catalog -rebuild
 # Set 'sources_1' fileset object
 set obj [get_filesets sources_1]
 set files [list \
-	[ file normalize "$origin_dir/hdl/vect2bits_16.v"]	\
+	[ file normalize "$origin_dir/hdl/vect2bits_9.v"]	\
 ]
 add_files -norecurse -fileset $obj $files
 
@@ -58,7 +58,7 @@ set files [list \
 add_files -fileset $obj $files
 
 # Source Block Design.
-set file "[file normalize "$origin_dir/bd/bd_4x2_2020-2.tcl"]"
+set file "[file normalize "$origin_dir/bd/bd_4x2_2022-1.tcl"]"
 source $file
 
 # Update compile order.
